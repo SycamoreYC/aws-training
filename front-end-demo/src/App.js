fefc4263a2ae5e058a63505c00cd3292482844fa
Yechun Song <yechun.song@rea-group.com>
2020-05-06 16:09:56 +0800

    change docker config

--- front-end-demo/src/App.js
+++ front-end-demo/src/App.js
@@ -6,20 +6,18 @@ function App() {
   const [userList, setUserList] = useState([])
   
   useEffect(() => {
-    fetch("/user/1").then(res => {
-      console.log(res);
-      
-      setUserList(res.data);
-    })
-  })
+    fetch("/user/all")
+    .then(res => res.json())
+    .then(result => setUserList(result))
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+        <ul style={{ color: '#fff' }}>
           {
-            userList.map(item => <li key={item.id}>{item.userName}</li>)
+            userList.map(item => <li style={{ color: '#fff' }} key={item.id}>{item.name}</li>)
           }
         </ul>
       </header>
